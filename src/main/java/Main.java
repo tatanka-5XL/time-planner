6b083a1e430b047ad51880e9c7467cacf849a534
petrpribil <petr.pribil@profisolv.com>
2022-11-30 08:45:28 +0100

    Beeps created

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        SoundUtils.tone(440,2000, 0.8);
+        LocalTime timeNow = LocalTime.now();
+
+
+    }
+}
