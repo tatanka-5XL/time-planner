3203e921ca496817b99217b8b3b0b7e3ccfcb7c0
petrpribil <petr.pribil@profisolv.com>
2022-11-30 09:43:20 +0100

    First functional version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,40 @@
 import java.time.LocalTime;
 
+// TODO add logging
 public class Main {
     public static void main(String[] args) throws Exception {
+        int work_period = 60 * 60 * 1000;
+        int lunch_period = 30 * 60 * 1000;
+        System.out.println(LocalTime.now() + " Good morning - your new great workday has started!");
         SoundUtils.tone(440,2000, 0.8);
-        LocalTime timeNow = LocalTime.now();
+        Thread.sleep(work_period);
+        System.out.println("Stretch!");
+        SoundUtils.tone(1000);
+        Thread.sleep(work_period);
+        System.out.println("Get moving!");
+        SoundUtils.doubleTone(1000);
+        Thread.sleep(work_period);
+        System.out.println("Stretch!");
+        SoundUtils.tone(1000);
+        Thread.sleep(work_period);
+        System.out.println("It's lunchtime - enjoy your meal!");
+        SoundUtils.tone(440,2000, 0.8);
 
+        Thread.sleep(lunch_period);
 
+        System.out.println("Back to work!");
+        SoundUtils.tone(440,2000, 0.8);
+        Thread.sleep(work_period);
+        System.out.println("Stretch!");
+        SoundUtils.tone(1000);
+        Thread.sleep(work_period);
+        System.out.println("Get moving!");
+        SoundUtils.doubleTone(1000);
+        Thread.sleep(work_period);
+        System.out.println("Stretch!");
+        SoundUtils.tone(1000);
+        Thread.sleep(work_period);
+        System.out.println(LocalTime.now() + " Good bye - time to go!");
+        SoundUtils.tone(440,2000, 0.8);
     }
 }
